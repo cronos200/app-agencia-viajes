d5e6e8846b05d680eec3ce558de865a079aa1238
jhon jairo <jhonjdv2022@outlook.com>
2025-04-20 21:10:03 -0500

    creacino del encarpetado, el componente login y susestilos a demas de intentar hacer el enrutamiento

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import PanelControl from './PanelControl.jsx'
 import Reservas from './Reservas.jsx'
+import Login from './pages/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import MenuLateral from "../MenuLateral";
+import "./login.css";
+
+function Login() {
+  return (
+    <div>
+      {/* <MenuLateral /> */}
+      <div className="login-background">
+        <div className="login-container">
+          <h2>Iniciar Sesión</h2>
+          <form>
+            <input type="email" placeholder="Correo electrónico" required />
+            <input type="password" placeholder="Contraseña" required />
+            <button type="submit">Ingresar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
